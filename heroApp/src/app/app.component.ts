8942fc5baea009b7a280d85c2c3e2bfb6b44bba4
A S M SARWAR ZAHAN <a.s.m.sarwar.zahan@gmail.com>
2017-09-10 21:34:37 -0400

    fresh start of module 4

--- heroApp/src/app/app.component.ts
+++ heroApp/src/app/app.component.ts
@@ -74,7 +74,7 @@ const HEROES: Hero[] = [
     <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
   </ul>
-  <hero-detail [hero]="selectedHero"></hero-detail>
+  <hero-detail [hero]="selectedHero" ></hero-detail>
   `
 })
 export class AppComponent {
